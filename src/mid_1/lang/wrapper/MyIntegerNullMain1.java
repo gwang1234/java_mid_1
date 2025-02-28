0e460d2895536ccbedc95638043d90ed4ca2de48
gwang1234 <qoi11@naver.com>
2025-02-28 15:50:36 +0900

    기본형의 한계

---
+++ src/mid_1/lang/wrapper/MyIntegerNullMain1.java
@@ -0,0 +1,21 @@
+package mid_1.lang.wrapper;
+
+public class MyIntegerNullMain1 {
+
+    public static void main(String[] args) {
+        MyInteger[] intArr = {new MyInteger(-1), new MyInteger(0), new MyInteger(1)};
+        System.out.println(findValue(intArr, -1));
+        System.out.println(findValue(intArr, 0));
+        System.out.println(findValue(intArr, 1));
+        System.out.println(findValue(intArr, 100));
+    }
+
+    private static MyInteger findValue(MyInteger[] intArr, int target) {
+        for (MyInteger MyInteger : intArr) {
+            if (MyInteger.getValue() == target) {
+                return MyInteger;
+            }
+        }
+        return null;
+    }
+}
